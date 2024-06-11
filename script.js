1b6608f7a63d7827bb1b097538332f1a3ccf63c5
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2024-06-11 14:31:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your RSVP!');
+    // You can add more functionality here, such as sending the form data to a server.
+});
+
