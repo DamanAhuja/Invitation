15df8e3532720f39b7001b3ddb5d987a4dca625a
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2024-06-11 15:03:12 +0530

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -1,6 +1,3 @@
-// script.js
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your RSVP!');
-    // You can add more functionality here, such as sending the form data to a server.
-});
+function showRSVPForm() {
+  document.getElementById("rsvpForm").style.display = "block";
+}
